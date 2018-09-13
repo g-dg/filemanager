b277266dd2aeb0746f9473e2497f14f612b83aaf
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-09-13 11:03:06 -0400

    Add documentation to the loader

--- server/loader.php
+++ server/loader.php
@@ -6,10 +6,21 @@ if (!defined('GARNETDG_FILEMANAGER')) {
 	exit();
 }
 
+class LoaderException extends Exception {}
+
 class Loader
 {
+	/**
+	 * The inits registerd through registerInit()
+	 */
 	protected static $registered_inits = [];
 
+	/**
+	 * Loads the specified directory
+	 * @param directory The directory (in relation to the current directory)
+	 * @return bool whether everything was loaded correctly
+	 * @throws LoaderException when the loader cannot load something
+	 */
 	public static function loadDirectory($directory) {
 		$directory = trim($directory, '/');
 
@@ -20,15 +31,20 @@ class Loader
 					if (is_readable($directory . '/' . $file)) {
 						require_once($directory . '/' . $file);
 					} else {
-						throw new \Exception('Loader could not read "' . $directory . '/' . $file . '".');
+						throw new LoaderException('Loader could not read "' . $directory . '/' . $file . '".');
 					}
 				}
 			}
 		} else {
-			throw new \Exception('Loader could not read "' . $directory . '".');
+			throw new LoaderException('Loader could not read "' . $directory . '".');
 		}
+		return true;
 	}
 
+	/**
+	 * Executes the inits registered with registerInit()
+	 * @return bool whether the inits were successfully executed
+	 */
 	public static function executeInits() {
 		krsort(self::$registered_inits);
 
@@ -37,14 +53,27 @@ class Loader
 				call_user_func($init_function);
 			}
 		}
+		return true;
 	}
 
+	/**
+	 * Clears the registered inits
+	 * @return bool whether all inits were cleared
+	 */
 	public static function clearInits() {
 		self::$registered_inits = [];
+		return true;
 	}
 
+	/**
+	 * Registers an init
+	 * @param function the init function to run
+	 * @param priority which priority to run the init at, higher priority runs first
+	 * @return bool whether the init was successfully registered
+	 */
 	public static function registerInit($function, $priority = 0)
 	{
 		self::$registered_inits[$priority][] = $function;
+		return true;
 	}
 }
