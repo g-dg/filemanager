170aca73dfa73c27ce0c453a1a96a65bc54d37b1
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-12-14 16:25:57 -0500

    Add basic client page

---
+++ client/html/index.php
@@ -0,0 +1,19 @@
+<?php
+namespace GarnetDG\FileManager;
+
+if (!defined('GARNETDG_FILEMANAGER')) {
+	http_response_code(403);
+	exit();
+}
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title><?= html_encode(GARNETDG_FILEMANAGER_NAME); ?></title>
+		
+	</head>
+	<body>
+		
+	</body>
+</html>
