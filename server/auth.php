b288c4449910f97de979e7716bf0eaaa1907472a
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-12-14 17:55:23 -0500

    Add more current user functions to auth

--- server/auth.php
+++ server/auth.php
@@ -6,13 +6,34 @@ if (!defined('GARNETDG_FILEMANAGER')) {
 	exit();
 }
 
-$current_user_id = null;
-
 /**
  * Get the current user ID, null if not logged in
  * @return int The current user ID
  */
 function auth_current_user_id() {
-	global $current_user_id;
-	return $current_user_id;
+	return session_get('auth.user.id');
+}
+
+/**
+ * Get the current user name, null if not logged in
+ * @return string The current user name
+ */
+function auth_current_user_name() {
+	return session_get('auth.user.name');
+}
+
+/**
+ * Get whether the current user is an administrator
+ * @return bool The current user administrator status
+ */
+function auth_current_user_administrator() {
+	return session_get('auth.user.administrator');
+}
+
+/**
+ * Get whether the current user is read-only
+ * @return bool The current user read-only status
+ */
+function auth_current_user_readonly() {
+	return session_get('auth.user.read_only');
 }
