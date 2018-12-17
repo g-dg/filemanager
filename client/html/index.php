4ad00bb138340d41b167d99447cdf38a9e4c362f
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-12-17 10:33:30 -0500

    Add more data tags to client page

--- client/html/index.php
+++ client/html/index.php
@@ -7,10 +7,16 @@ if (!defined('GARNETDG_FILEMANAGER')) {
 }
 ?><!DOCTYPE html>
 <html lang="en">
-	<head data-application_static_root_path="<?= html_encode(http_encode_path(get_application_http_root_path())); ?>/client" data-application_dynamic_root_path="<?= html_encode(http_encode_path(get_application_http_script_path())); ?>">
+	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta name="application-name" content="<?= html_encode(GARNETDG_FILEMANAGER_NAME); ?>" />
+		<meta name="application-name" content="<?= html_encode(GARNETDG_FILEMANAGER_NAME); ?>"
+			data-copyright="<?= GARNETDG_FILEMANAGER_COPYRIGHT_HTML; ?>"
+			data-version="<?= html_encode(GARNETDG_FILEMANAGER_VERSION); ?>"
+			data-debug="<?= GARNETDG_FILEMANAGER_DEBUG_ENABLE ? 'true' : 'false'; ?>"
+			data-application_static_root_path="<?= html_encode(http_encode_path(get_application_http_root_path())); ?>/client"
+			data-application_dynamic_root_path="<?= html_encode(http_encode_path(get_application_http_script_path())); ?>"
+			data-application_api_path="<?= html_encode(http_encode_path(get_application_http_script_path())); ?>/api" />
 		<title><?= html_encode(GARNETDG_FILEMANAGER_NAME); ?></title>
 		<link rel="shortcut icon" href="<?= html_encode(http_encode_path(get_application_http_root_path())); ?>/client/img/favicon.ico" />
 		<link rel="stylesheet" href="<?= html_encode(http_encode_path(get_application_http_root_path())); ?>/client/css/normalize.css" />
