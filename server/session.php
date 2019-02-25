47d4a24d1e343d339f0a7ba6447dfd327f5a43f5
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2019-02-25 11:53:04 -0500

    Hard-code session id length and characters

--- server/session.php
+++ server/session.php
@@ -10,6 +10,8 @@ class SessionException extends Exception {}
 class SessionNotStartedException extends SessionException {}
 
 define('GARNETDG_FILEMANAGER_SESSION_PARAMETER', '_session_id');
+define('GARNETDG_FILEMANAGER_SESSION_ID_LENGTH', 255);
+define('GARNETDG_FILEMANAGER_SESSION_ID_CHARACTERS', 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789');
 
 /**
  * The session id for the current request, null if not set
@@ -50,7 +52,7 @@ function session_start($sessid = null, $regenerate_on_failure = false)
 				// check if the session is valid
 				if ((int)database_query('SELECT COUNT() FROM "sessions" WHERE "id" = ? AND "last_used" > ?;', [(string)$session_id], time() - settings_get_system('session.age.max'))[0][0] == 0) {
 					if ($generate_new_on_failure) {
-						$session_id = generate_random_string(settings_get_system('session.id.length'), settings_get_system('session.id.chars'));
+						$session_id = generate_random_string(GARNETDG_FILEMANAGER_SESSION_ID_LENGTH, GARNETDG_FILEMANAGER_SESSION_ID_CHARACTERS);
 						database_query('INSERT INTO "sessions"("id") VALUES (?);', [$session_id]);
 					} else {
 						return false;
@@ -61,7 +63,7 @@ function session_start($sessid = null, $regenerate_on_failure = false)
 				}
 			} else {
 				// generate a new session
-				$session_id = generate_random_string(settings_get_system('session.id.length'), settings_get_system('session.id.chars'));
+				$session_id = generate_random_string(GARNETDG_FILEMANAGER_SESSION_ID_LENGTH, GARNETDG_FILEMANAGER_SESSION_ID_CHARACTERS);
 				database_query('INSERT INTO "sessions"("id") VALUES (?);', [$session_id]);
 			}
 		}
