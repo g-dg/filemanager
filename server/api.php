3e049f9964741e320beb73bfd459aca3ffd4e934
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2019-06-15 11:52:58 -0400

    Fill out a bunch of the API

--- server/api.php
+++ server/api.php
@@ -20,10 +20,40 @@ function exec_api($request)
 		return false;
 	}
 
+	// the majority of these will return JSON
+	header('Content-Type: application/json');
+
+	$session_id = null;
+	if (isset($_GET['_sessid']))
+		$session_id = $_GET['_sessid'];
+	if (isset($_POST['_sessid']))
+		$session_id = $_POST['_sessid'];
+
+	// start session if required
+	if (!($api_request[0] === 'login')) {
+		if (!is_null($session_id)) {
+			session_start($session_id);
+		} else {
+			log(LOG_ERR, 'Session ID not passed to api endpoint that requires it', 'api');
+			http_response_code(400);
+			return false;
+		}
+	}
+
 	switch ($api_request[0]) {
 
 		case 'login':
-
+			if (isset($_POST['username'], $_POST['password'])) {
+				
+			} else {
+				http_response_code(400);
+				return false;
+			}
+			break;
+		
+		case 'logout':
+			session_start($session_id);
+			session_destroy();
 			break;
 		
 		case 'users':
@@ -39,19 +69,103 @@ function exec_api($request)
 			break;
 
 		case 'filesystem':
+			if (count($api_request) >= 2) {
+				switch ($api_request[1]) {
+					case 'copy':
 
+						break;
+					case 'create':
+
+						break;
+					case 'delete':
+
+						break;
+					case 'mkdir':
+
+						break;
+					case 'read':
+
+						break;
+					case 'readdir':
+
+						break;
+					case 'rename':
+
+						break;
+					case 'rmdir':
+
+						break;
+					case 'stat':
+
+						break;
+					case 'truncate':
+
+						break;
+					case 'unlink':
+
+						break;
+					case 'write':
+
+						break;
+					default:
+						http_response_code(404);
+						return false;
+				}
+			} else {
+				http_response_code(404);
+				return false;
+			}
 			break;
 
 		case 'application':
+			if (count($api_request) >= 2) {
+				switch ($api_request[1]) {
+					case 'settings':
 
-			break;
+						break;
+					case 'info':
 
-		case 'sessions':
-
+						break;
+					default:
+						http_response_code(404);
+						return false;
+				}
+			} else {
+				http_response_code(404);
+				return false;
+			}
 			break;
 
 		case 'administration':
+			if (!auth_current_user_administrator()) {
+				http_response_code(403);
+				return false;
+			}
+			if (count($api_request) >= 2) {
+				switch ($api_request[1]) {
+					case 'search_index_rebuild':
 
+						break;
+					case 'search_index_clear':
+						database_lock();
+						database_query('DELETE FROM "search_index_keywords";');
+						database_query('DELETE FROM "search_index_entries";');
+						database_unlock();
+						break;
+					case 'database_analyze':
+						database_query('ANALYZE;');
+						break;
+					case 'database_vacuum':
+						database_query('VACUUM;');
+						break;
+					default:
+						http_response_code(404);
+						return false;
+				}
+			} else {
+				http_response_code(404);
+				return false;
+			}
 			break;
 		
 		case 'search':
@@ -62,4 +176,5 @@ function exec_api($request)
 			http_response_code(404);
 			return false;
 	}
+	return true;
 }
